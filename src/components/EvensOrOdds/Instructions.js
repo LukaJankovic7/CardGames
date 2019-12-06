a8aa6d697be4bf4f15aa562636e72d55f6c1c640
Luka Jankovic <luka.jank998@gmail.com>
2019-12-06 11:14:33 +0100

    Remove test code

--- src/components/EvensOrOdds/Instructions.js
+++ src/components/EvensOrOdds/Instructions.js
@@ -2,14 +2,13 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {expandInstructions} from '../../actions/settings';
 import {collapseInstructions} from '../../actions/settings';
-import InstructionsStyles from './styles/instructions.scss';
 
 const Instructions = props => {
     const {instructionsExpanded, expandInstructions, collapseInstructions} = props;
 
     return(
         instructionsExpanded ? (
-            <div className={InstructionsStyles.wrapper}>
+            <div>
                 <p>Instructions: </p>
                 <p>Guess if the card is even or odd!</p>
                 <p>Click on the responding buttons to make your guess</p>
@@ -18,7 +17,7 @@ const Instructions = props => {
                 <button onClick = {collapseInstructions}>Show less</button>
             </div>
         ) : (
-            <div className={InstructionsStyles.wrapper}>
+            <div>
                 <p>Instructions: </p>
                 <p>Guess if the next card is even or odd!</p>
                 <button onClick = {expandInstructions}>Show more</button>
